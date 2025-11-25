7ac1ce48fe8bc9fb1ed1d9affa6fefb7b9414e8d
karub360 <karub360@gmail.com>
2025-11-26 01:39:42 +0300

    feat(navigation): React Router DOM entegrasyonu ve modüler rota yapısı oluşturuldu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
+import AppRouter from './navigation/AppRouter';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <AppRouter />
     </div>
   );
 }
