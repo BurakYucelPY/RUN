ede26f247c5b5228cbbb251bdfa065a2cae7adaa
karub360 <karub360@gmail.com>
2025-11-25 23:05:39 +0300

    refactor(app): Ana sayfa yönlendirmesi Home bileşenine güncellendi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      {/* Temiz başlangıç - RUN Oyunu */}
+      <Home />
     </div>
   );
 }
