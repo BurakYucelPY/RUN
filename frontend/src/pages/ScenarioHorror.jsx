cac94feb4cf7ac99af61db0b53b169da23458965
karub360 <karub360@gmail.com>
2025-11-27 06:13:52 +0300

    feat(ui): Korku senaryo sayfası eklendi

---
+++ frontend/src/pages/ScenarioHorror.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import LightRays from '../components/LightRays';
+
+const ScenarioHorror = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh', position: 'relative', backgroundColor: '#000' }}>
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#ff0000"
+          raysSpeed={1.5}
+          lightSpread={0.8}
+          rayLength={1.2}
+          fadeDistance={2.0}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ScenarioHorror;
